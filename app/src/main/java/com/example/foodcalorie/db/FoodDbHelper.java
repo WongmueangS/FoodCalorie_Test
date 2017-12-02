8a9504df40c6de8ce9c956ab3b00309beaefb65f
WongmueangS <nand.supatta@gmail.com>
2017-12-02 16:18:43 +0700

    Update FoodDbHelper.java

--- app/src/main/java/com/example/foodcalorie/db/FoodDbHelper.java
+++ app/src/main/java/com/example/foodcalorie/db/FoodDbHelper.java
@@ -5,29 +5,24 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-/**
- * Created by ADMIN on 29/11/2560.
- */
-
 public class FoodDbHelper extends SQLiteOpenHelper {
-    private static final String DATABASE_NAME = "food.db";
-    private static final int DATABASE_VERSION =1;
+    private static final String DATABASE_NAME = "food.db"; //ชื่อdatabase
+    private static final int DATABASE_VERSION = 1; //เวอร์ชันของ database
 
-    public static final String TABLE_NAME = "Food";
+    public static final String TABLE_NAME = "Food";  //ชื่อตาราง
     public static final String COL_ID = "_id";
-    public static final String COL_TITLE = "_title";
-    public static final String COL_Calorie = "_calorie";
-    public static final String COL_Type = "_type";
-    public static final String COL_PICTURE = "_picture";
+    public static final String COL_TITLE = "_title"; // ชื่ออาหาร
+    public static final String COL_Calorie = "_calorie"; //ปรมาณแคลอรี่ในอาหาร
+    public static final String COL_Type = "_type"; // ประเภทของอาหาร
+    public static final String COL_PICTURE = "_picture"; //ภาพอาหาร
 
-
-    //คำสั่งสร่าง table
-    private static  final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+    //คำสั่งในการสร้าง table
+    private static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
             + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
             + COL_TITLE + " TEXT, "
             + COL_Calorie + " TEXT, "
             + COL_Type + " TEXT, "
-            + COL_PICTURE + " TEXT)" ;
+            + COL_PICTURE + " TEXT)";
 
 
     public FoodDbHelper(Context context) {
@@ -36,141 +31,126 @@ public class FoodDbHelper extends SQLiteOpenHelper {
 
     //ctrl + o สร้าง onCreate
     @Override
-    public void onCreate(SQLiteDatabase db) { //เรียกดูว่า ฐานข้อมูลถูกสร้างหรือยัง //ทำแค่ครั้งเดียวตอนที่ฐานข้อมูลยังไม่ถูกสร้าง
-        db.execSQL(CREATE_TABLE);//คำสั่งสร้างtable
-        //กรณีนี้เราต้องเตรียมเบอร์เทอร์เบื้องต้นไว้ให้ user ก่อน ดังนั้นเราต้องinsertข้อมูล เริ่มต้น
-        insertInitialData(db); //insert ข้อมูล
+    public void onCreate(SQLiteDatabase db) { //เรียกดูว่า ฐานข้อมูลถูกสร้างหรือยัง ( ทำแค่ครั้งเดียวตอนที่ฐานข้อมูลยังไม่ถูกสร้าง )
+        db.execSQL(CREATE_TABLE); //คำสั่งสร้างtable
+        insertInitialData(db); //insert ข้อมูล ลงตาราง
     }
 
     private void insertInitialData(SQLiteDatabase db) {
+        //ข้อมูล ขนมหวาน ประเภทที่ 1
         ContentValues cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"กล้วยบวชชี 1 ถ้วย");
-        cv.put(COL_Calorie,"230 Kcal");
-        cv.put(COL_Type,"1");
-        cv.put(COL_PICTURE,"S001.jpg");
-        db.insert(TABLE_NAME,null,cv);
+        //putค่าต่างๆใส่ลงcv
+        cv.put(COL_TITLE, "กล้วยบวชชี 1 ถ้วย"); //ชื่อฟิล,ค่าที่ใส่
+        cv.put(COL_Calorie, "230 Kcal");
+        cv.put(COL_Type, "1");
+        cv.put(COL_PICTURE, "S001.jpg");
+        db.insert(TABLE_NAME, null, cv); //เอาcv ใส่ลงฐานข้อมูล
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"กุ่ยช่าย(นึ่ง) 1 ชิ้น");
-        cv.put(COL_Calorie,"140 Kcal");
-        cv.put(COL_Type,"1");
-        cv.put(COL_PICTURE,"S002.jpg");
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "กุ่ยช่าย(นึ่ง) 1 ชิ้น");
+        cv.put(COL_Calorie, "140 Kcal");
+        cv.put(COL_Type, "1");
+        cv.put(COL_PICTURE, "S002.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ขนมครก 2 คู่");
-        cv.put(COL_Calorie,"210 Kcal");
-        cv.put(COL_Type,"1");
-        cv.put(COL_PICTURE,"S003.jpg");
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "ขนมครก 2 คู่");
+        cv.put(COL_Calorie, "210 Kcal");
+        cv.put(COL_Type, "1");
+        cv.put(COL_PICTURE, "S003.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ขนมชั้น 2 ชิ้น");
-        cv.put(COL_Calorie,"184 Kcal");
-        cv.put(COL_Type,"1");
-        cv.put(COL_PICTURE,"S004.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "ขนมชั้น 2 ชิ้น");
+        cv.put(COL_Calorie, "184 Kcal");
+        cv.put(COL_Type, "1");
+        cv.put(COL_PICTURE, "S004.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ขนมตาล 2 กระทง");
-        cv.put(COL_Calorie,"115 Kcal");
-        cv.put(COL_Type,"1");
-        cv.put(COL_PICTURE,"S005.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "ขนมตาล 2 กระทง");
+        cv.put(COL_Calorie, "115 Kcal");
+        cv.put(COL_Type, "1");
+        cv.put(COL_PICTURE, "S005.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         //---------------------------------------------
-
-       cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ข้าวผัดรวมมิตรทะเล");
-        cv.put(COL_Calorie,"660 Kcal");
-        cv.put(COL_Type,"2");
-        cv.put(COL_PICTURE,"001.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        //ข้อมูล อาหารคาว ประเภทที่ 2
+        cv = new ContentValues(); //สร้าง ContentValues
+        cv.put(COL_TITLE, "ข้าวผัดรวมมิตรทะเล");
+        cv.put(COL_Calorie, "660 Kcal");
+        cv.put(COL_Type, "2");
+        cv.put(COL_PICTURE, "001.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"กะเพราะไก่-ไข่ดาว");
-        cv.put(COL_Calorie,"630 Kcal");
-        cv.put(COL_Type,"2");
-        cv.put(COL_PICTURE,"002.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
-
-        cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ข้าวมันไก่");
-        cv.put(COL_Calorie,"596 Kcal");
-        cv.put(COL_Type,"2");
-        cv.put(COL_PICTURE,"003.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "กะเพราะไก่-ไข่ดาว");
+        cv.put(COL_Calorie, "630 Kcal");
+        cv.put(COL_Type, "2");
+        cv.put(COL_PICTURE, "002.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ข้าวขาหมู");
-        cv.put(COL_Calorie,"690 Kcal");
-        cv.put(COL_Type,"2");
-        cv.put(COL_PICTURE,"004.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "ข้าวมันไก่");
+        cv.put(COL_Calorie, "596 Kcal");
+        cv.put(COL_Type, "2");
+        cv.put(COL_PICTURE, "003.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ข้าวคลุกกะปิ");
-        cv.put(COL_Calorie,"410 Kcal");
-        cv.put(COL_Type,"2");
-        cv.put(COL_PICTURE,"005.jpg");
+        cv.put(COL_TITLE, "ข้าวขาหมู");
+        cv.put(COL_Calorie, "690 Kcal");
+        cv.put(COL_Type, "2");
+        cv.put(COL_PICTURE, "004.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
-        db.insert(TABLE_NAME,null,cv);
+        cv = new ContentValues(); //สร้าง ContentValues
+        cv.put(COL_TITLE, "ข้าวคลุกกะปิ");
+        cv.put(COL_Calorie, "410 Kcal");
+        cv.put(COL_Type, "2");
+        cv.put(COL_PICTURE, "005.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         //-----------------------------------------
+        //ข้อมูล ผลไม้ ประเภทที่ 3
+        cv = new ContentValues(); //สร้าง ContentValues
+        cv.put(COL_TITLE, "กล้วยน้ำว้า 1 ผล");
+        cv.put(COL_Calorie, "60 Kcal");
+        cv.put(COL_Type, "3");
+        cv.put(COL_PICTURE, "S001.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"กล้วยน้ำว้า 1 ผล");
-        cv.put(COL_Calorie,"60 Kcal");
-        cv.put(COL_Type,"3");
-        cv.put(COL_PICTURE,"S001.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
-
-        cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"กล้วยหอม 1 ผล");
-        cv.put(COL_Calorie,"120 Kcal");
-        cv.put(COL_Type,"3");
-        cv.put(COL_PICTURE,"S002.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "กล้วยหอม 1 ผล");
+        cv.put(COL_Calorie, "120 Kcal");
+        cv.put(COL_Type, "3");
+        cv.put(COL_PICTURE, "S002.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ขนุน 2 ยวง");
-        cv.put(COL_Calorie,"60 Kcal");
-        cv.put(COL_Type,"3");
-        cv.put(COL_PICTURE,"S003.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
-
-        cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ชมพู่ 2-3 ผล ");
-        cv.put(COL_Calorie,"60 Kcal");
-        cv.put(COL_Type,"3");
-        cv.put(COL_PICTURE,"S004.jpg");
-
-        db.insert(TABLE_NAME,null,cv);
+        cv.put(COL_TITLE, "ขนุน 2 ยวง");
+        cv.put(COL_Calorie, "60 Kcal");
+        cv.put(COL_Type, "3");
+        cv.put(COL_PICTURE, "S003.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
         cv = new ContentValues(); //สร้าง ContentValues
-        cv.put(COL_TITLE,"ทุเรียน 100 กรัม");
-        cv.put(COL_Calorie,"165 Kcal");
-        cv.put(COL_Type,"3");
-        cv.put(COL_PICTURE,"S005.jpg");
+        cv.put(COL_TITLE, "ชมพู่ 2-3 ผล ");
+        cv.put(COL_Calorie, "60 Kcal");
+        cv.put(COL_Type, "3");
+        cv.put(COL_PICTURE, "S004.jpg");
+        db.insert(TABLE_NAME, null, cv);
 
-        db.insert(TABLE_NAME,null,cv);
-
-
-
-
+        cv = new ContentValues(); //สร้าง ContentValues
+        cv.put(COL_TITLE, "ทุเรียน 100 กรัม");
+        cv.put(COL_Calorie, "165 Kcal");
+        cv.put(COL_Type, "3");
+        cv.put(COL_PICTURE, "S005.jpg");
+        db.insert(TABLE_NAME, null, cv);
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME); //สั่งให้ลบเมื่อมีtable นี้อยู่
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME); //สั่งให้ลบเมื่อมี table นี้อยู่
         onCreate(db); //สร้าง table ใหม่
     }
 
