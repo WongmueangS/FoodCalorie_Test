2495ad3700ff2bbab2f4430206cbf0ef6a39d1b5
WongmueangS <nand.supatta@gmail.com>
2017-12-02 16:20:06 +0700

    Update Path2.java

--- app/src/main/java/com/example/foodcalorie/Path2.java
+++ app/src/main/java/com/example/foodcalorie/Path2.java
@@ -1,16 +1,10 @@
 package com.example.foodcalorie;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.foodcalorie.adapter.FoodListAdapter;
@@ -19,14 +13,11 @@ import com.example.foodcalorie.model.FoodItem;
 
 import java.util.ArrayList;
 
-
-
 public class Path2 extends AppCompatActivity {
-
 
     private FoodDbHelper mHelper; //เข้าถึงฐานข้อมูล
     private SQLiteDatabase mDb; //ตัวอ้างอิงฐานข้อมูล
-    private  String type;
+    private String type;
 
     //สร้าง Arrylist
     private ArrayList<FoodItem> mFoodItemList = new ArrayList<>();
@@ -38,11 +29,11 @@ public class Path2 extends AppCompatActivity {
         setContentView(R.layout.activity_path2);
 
         Intent intent = getIntent();
-         type = intent.getStringExtra("type");
+        type = intent.getStringExtra("type"); // รับค่าจาก Activity ต้นทาง
 
         mHelper = new FoodDbHelper(this);
-        mDb =  mHelper.getReadableDatabase();  //เรียกmethod ReadableDatabase เพื่อเข้าถึงฐานข้อมูล
-        loadDataFromDb();
+        mDb = mHelper.getReadableDatabase();  // เรียกmethod ReadableDatabase เพื่อเข้าถึงฐานข้อมูล
+        loadDataFromDb(); //จะคิวรีข้อมูล ทุกแถว ทุกคอลัม
 
         //ผูก listview กับ แหล่งข้อมูล
         mAdapter = new FoodListAdapter(
@@ -54,37 +45,33 @@ public class Path2 extends AppCompatActivity {
 
         ListView lv = (ListView) findViewById(R.id.list_view);
         lv.setAdapter(mAdapter);
-
-
-
-
     }
 
     private void loadDataFromDb() {
         //ดึงข้อมูลจากฐานข้อมูล
-        Cursor cursor = mDb.query(  //สร้างตัวแปร cursor เพื่อมารับค่า //cursor มองว่าเแป็นตารางในฐานข้อมูล
+        Cursor cursor = mDb.query(  //สร้างตัวแปร cursor เพื่อมารับค่า (cursor มองว่าเแป็นตารางในฐานข้อมูล)
                 FoodDbHelper.TABLE_NAME,
                 null, //เอาทั้งแถว
-                FoodDbHelper.COL_Type + "="+type , //กรณีที่มีเเงื่อนไข "ชื่อฟิล = 1,
-                //null,
+                FoodDbHelper.COL_Type + "=" + type, // คิวรี่เฉพาะคอลัมType ที่มีค่า = Type  //กรณีที่มีเเงื่อนไข "ชื่อฟิล + "=" + (ค่าที่ต้องการ),
                 null,
                 null,
                 null,
                 null
         );
 
+        mFoodItemList.clear();// clear ข้อมูลเก่าทิ้ง (เผื่อไว้กรณีผู้ใช้แอดข้อมูลเพิ่มมา)
 
+        while (cursor.moveToNext()) { //วนloop เพื่อดึงข้อมูลในแต่ละแถว (ตัวชี้)
 
-        while (cursor.moveToNext()){ //วนloop เพื่อดึงข้อมูลในแต่ละแถว //ตัวชี้
-
-            int id =  cursor.getInt(cursor.getColumnIndex(FoodDbHelper.COL_ID));
-            String title =  cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_TITLE));
-            String calorie =  cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_Calorie));
-            String type =  cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_Type));
+            int id = cursor.getInt(cursor.getColumnIndex(FoodDbHelper.COL_ID));
+            String title = cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_TITLE));
+            String calorie = cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_Calorie));
+            String type = cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_Type));
             String picture = cursor.getString(cursor.getColumnIndex(FoodDbHelper.COL_PICTURE));
 
-            FoodItem item = new FoodItem(id, title,calorie,type,picture);//เอาทั้ง 4 ตัวเใส่ข้าไปในmodel
-            mFoodItemList.add(item);
+            //สร้างโมเดลobj โดยผ่านconstructor ของFoodItem ที่สร้างไว้
+            FoodItem item = new FoodItem(id, title, calorie, type, picture);//เอาทั้ง 5 ตัว ใส่เข้าไปในmodel
+            mFoodItemList.add(item); //ข้อมูลจากdb มาอยู่ในนี้หมดแล้ว
 
         }
     }
